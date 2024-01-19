da9600826d4da1fcf049e8ac74a70fe1a0260964
chrispei1101 <peipei@ucdavis.edu>
2024-01-19 14:12:28 -0800

    addyawncount

--- face_detector/sleepy_detector.py
+++ face_detector/sleepy_detector.py
@@ -6,11 +6,13 @@ import time
 head_not_forward_count = 0
 nod_count = 0
 blink_count = 0
+yawn_count = 0
 face_detected = False
 one_sec = 30
 eye_closed_frame = 0
 start_time = time.time()
 frame_count = 0
+yawn_frame = 0
 
 #work cited: TianjingWu https://github.com/TianxingWu for detecting faces
 
@@ -33,14 +35,15 @@ cap = cv2.VideoCapture(0) # connect to your camera
 queue = np.zeros(30,dtype=int)
 queue = queue.tolist()
 frames_since_last_blink = 0
+frames_since_last_yawn = 0
+
 
 while(cap.isOpened()):
     
-
-
     face_detected = False
     blink_detected = False
     frames_since_last_blink += 1
+    frames_since_last_yawn += 1
     frame_count += 1
     
     # read frame
@@ -48,10 +51,10 @@ while(cap.isOpened()):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 1)
     cv2.putText(img, "Blink Count: {}".format(blink_count), (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
-    
+    cv2.putText(img, "Yawn Count: {}".format(yawn_count), (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
     elapsed_time = time.time() - start_time
     if elapsed_time >= 1.0:
-        fps = frame_count / elapsed_time
+        fps = frame_count
         start_time = time.time()
         frame_count = 0
         
@@ -76,8 +79,8 @@ while(cap.isOpened()):
         left_eye_box = landmarks[36:42].astype(int)
         right_eye_box = landmarks[42:48].astype(int)
 
-        cv2.polylines(img, [left_eye_box], isClosed=True, color=(255, 0, 0), thickness=2)
-        cv2.polylines(img, [right_eye_box], isClosed=True, color=(255, 0, 0), thickness=2)
+        cv2.polylines(img, [left_eye_box], isClosed=True, color=(255, 255, 255), thickness=1)
+        cv2.polylines(img, [right_eye_box], isClosed=True, color=(255, 255, 255), thickness=1)
 
      
         # eye distance
@@ -91,19 +94,14 @@ while(cap.isOpened()):
         d_reference = (d5+d6)/2
         d_judge = d_mean/d_reference
         
-        if d_judge<0.2 and frames_since_last_blink>one_sec/3:
+        if d_judge<0.2 and frames_since_last_blink>one_sec/4:
             blink_count += 1
             frames_since_last_blink = 0
         
         flag = int(d_judge<0.2) #0.2 is the threshod, adjust bigger if you have larger eyes unlike me
 
         queue = queue[1:len(queue)] + [flag] #enqueue flag
-        
-        # if more than half surpass threshod
-        '''
-        if sum(queue) > len(queue)/2 :
-            cv2.putText(img, "Driver Eye Closed !", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)   
-        '''
+
         
         # Count blinks
         if sum(queue) > len(queue)/2:
@@ -111,13 +109,14 @@ while(cap.isOpened()):
                     
         mouth_open_threshold = 10  # Adjust the threshold as needed
         if landmarks[66][1] - landmarks[62][1] > mouth_open_threshold:
-            yawn_count += 1
-            if yawn_count >= 10: #if surpass 10 frames
-                cv2.putText(img, "Driver Yawning!", (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
-        else:
-            yawn_count = 0
+            yawn_frame += 1
+            if yawn_frame >= 10 and frames_since_last_blink>one_sec*3: #if surpass 10 frames and more than one secend
+                yawn_count += 1
+                frames_since_last_yawn = 0
+                yawn_frame = 0
+            
 
-        if head_not_forward_count == 0 and yawn_count == 0 and sum(queue) <= len(queue)/2:
+        if sum(queue) <= len(queue)/2:
             cv2.putText(img, "SAFE", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
             
     if not face_detected:
